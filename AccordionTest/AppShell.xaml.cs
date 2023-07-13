67e0501be1e41e1d9d5eface822bcd339d8c33b5
L3oJr <leojuniortj@gmail.com>
2023-07-13 17:09:26 -0300

    First commit

---
+++ AccordionTest/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AccordionTest;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AccordionTest/MainPageViewModel.cs
@@ -0,0 +1,43 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace AccordionTest
+{
+    public partial class MainPageViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        bool firstCardOpened;
+
+        [ObservableProperty]
+        bool secondCardOpened;
+
+        [ObservableProperty]
+        bool thirdCardOpened;
+
+        [ObservableProperty]
+        bool fourthCardOpened;
+
+        [RelayCommand]
+        void CardToggle(string toggleName)
+        {
+            switch (toggleName)
+            {
+                case "first":
+                    this.FirstCardOpened = !this.FirstCardOpened;
+                    break;
+                case "second": 
+                    this.SecondCardOpened = !this.SecondCardOpened;
+                    break;
+                case "third":
+                    this.ThirdCardOpened = !this.ThirdCardOpened;
+                    break;
+                case "fourth":
+                    this.FourthCardOpened = !this.FourthCardOpened;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+}
